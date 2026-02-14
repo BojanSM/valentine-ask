f34cb456fe46838c555fb0a4de93c097d9df1b04
Bojan Nenadovic <bojannenadovic@gmail.com>
2026-02-14 02:33:47 +0100

    A few renamings

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "Yoohoo, znao sam da me voliš!";
 
     catImg.src = "cat_dance.gif";
 
